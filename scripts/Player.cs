58f53a7a7789ddbc2f148cd3bfc23db6956d56a3
Luis Gonzalez <interactivelabsus@gmail.com>
2025-06-03 12:05:02 -0700

    Start a new tutorial see what's up

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,12 +4,20 @@ public partial class Player : CharacterBody2D
 {
 	public const float _Speed = 100.0f;
 
-	private int _currentRow = 0;
-
 	// TODO: find out how to share this between the two scripts
 	const int _TextureSize = 256;
 	const float _TileScaleSize = 0.078125f;
-	const float _TileOffsetSize = _TextureSize * _TileScaleSize;
+	const float _RowSize = _TextureSize * _TileScaleSize;
+
+	enum PlayerRow
+	{
+		Top = 1,
+		Middle = 0,
+		Bottom = -1
+	}
+
+	PlayerRow _currentRow = PlayerRow.Middle;
+	PlayerRow _targetRow = PlayerRow.Middle;
 
 
 	public override void _PhysicsProcess(double delta)
@@ -17,27 +25,30 @@ public partial class Player : CharacterBody2D
 
 		base._PhysicsProcess(delta);
 
-		if (Input.IsActionJustPressed("ui_up") && _currentRow < 2)
+		if (Input.IsActionJustPressed("ui_up") && _currentRow < PlayerRow.Top)
 		{
 			_currentRow += 1;
 		}
-		else if (Input.IsActionJustPressed("ui_down") && _currentRow > 0)
+		else if (Input.IsActionJustPressed("ui_down") && _currentRow > PlayerRow.Bottom)
 		{
 			_currentRow -= 1;
 		}
 
-		var TargetPosition = Vector2.Up * _currentRow * _TileOffsetSize;
-		var direction = Position.DirectionTo(TargetPosition).Normalized();
-		var velocity = direction * _Speed;
+		float targetY = (int)_targetRow * _RowSize;
+		Position = Position.Lerp(new Vector2(Position.X, targetY), _Speed * (float)delta);
 
-		if (Position.DistanceTo(TargetPosition) > 1)
-		{
-			Velocity = velocity;
-		}
-		else
-		{
-			Velocity = Vector2.Zero;
-		}
+		// var TargetPosition = Vector2.Up * _currentRow * _RowSize;
+		// var direction = Position.DirectionTo(TargetPosition).Normalized();
+		// var velocity = direction * _Speed;
+
+		// if (Position.DistanceTo(TargetPosition) > 1)
+		// {
+		// 	Velocity = velocity;
+		// }
+		// else
+		// {
+		// 	Velocity = Vector2.Zero;
+		// }
 
 		MoveAndSlide();
 	}
